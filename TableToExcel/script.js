698a18bd5841c7f13811803680b765db72bbfff9
CleberLeivas <crebis@hotmail.com>
2024-03-06 17:08:27 -0300

    boa

--- TableToExcel/script.js
+++ TableToExcel/script.js
@@ -27,4 +27,33 @@ function adicionarItem(){
     document.getElementById("quantidade").value = ""
 }
 
+function exportarParaExcel(){
+    var tabela = document.getElementById("tabela");
+    var nomeArquivo = "tabela_produtos.xlsx";
+    var wb = XLSX.utils.table_to_book(tabela, {sheet: "Tabela de Produtos"});
+    XLSX.writeFile(wb, nomeArquivo);
+}
 
+function removerItem(){
+    var nomeParaRemover = document.getElementById("nomeRemover").value
+    if(!nomeParaRemover){
+        alert("Digite um nome")
+        return;
+    }
+
+    var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0]
+    var linhas = tabela.getElementsByTagName("tr");
+
+    for(var i = 0; i < linhas.length; i++){
+        // obtendo a primeira celula (td) da linha atual
+        var celulaNome = linhas[i].getElementsByTagName("td")[0];
+        
+        //vericando se a celula nome existe e se o conteudo eh igual ao que quer
+        if(celulaNome && celulaNome.innerHTML === nomeParaRemover){
+            tabela.deleteRow(i)
+            document.getElementsByTagName("nomeRemover").value = ""
+            return;
+        }
+        alert("Digite um nome existente na tabela")
+    }
+}
